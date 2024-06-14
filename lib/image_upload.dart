abebde20b00ff33d40a01b14bd6fd393791a4794
Nikhilbhashapangu <nikhilbhashapangu@gmail.com>
2024-06-14 12:26:28 +0530

    Cloud change

--- lib/image_upload.dart
+++ lib/image_upload.dart
@@ -85,7 +85,7 @@ class _ImageUploadScrenState extends State<ImageUploadScren> {
                       height: 5.5.h,
                       borderRadius: 2.w,
                       color: const Color(0xffD3D3D6),
-                      label: "Local Main Changed",
+                      label: "Local Main Changed by Cloud main",
                       onPressed: () {
                         setState(() {
                           showImage = true;
